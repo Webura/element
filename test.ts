8035bd6c43a6762f65633ce772cecc4e1165d636
webura <johnny@webura.com>
2021-08-01 20:59:34 +0800

    improved test

--- test.ts
+++ test.ts
@@ -1,22 +1,24 @@
 import Element from "./index";
 const html = `
-<html>
-<head></head>
-<body>
-  <div id='books'>
-    <ul>
-      <li class='book'>Book1</li>
-      <li class='book'>Book2</li>
-      <li class='book' style='display:none'>Hidden book</li>
-      <li class='book'>Book3</li>
-    </ul>
-  </div>
-</body>
-</html>
-`;
+  <html>
+  <head></head>
+  <body>
+    <div id='books'>
+      <ul>
+        <li class='book' data-year='2001'>Book1</li>
+        <li class='book' data-year='2002'>Book2</li>
+        <li class='book' data-year='2000' style='display:none'>Hidden book</li>
+        <li class='book' data-year='2003'>Book3</li>
+      </ul>
+    </div>
+  </body>
+  </html>`;
 const rootElement = Element.parse(html);
 const booksDiv = rootElement.find(el => el.tag == 'div' && el.attr.id == 'books')!;
 for (const bookElement of booksDiv.filter(el => el.attr.class == 'book')) {
   if (bookElement.attr.style != 'display:none')
-    console.log(bookElement.value);
+    console.log(bookElement.value + ': ' + bookElement.attr['data-year']);
 }
+// Book1: 2001
+// Book2: 2002
+// Book3: 2003
